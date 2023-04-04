c85f078fb0f232f4ff3694e1091407fd1704e345
Praghadesh-7 <727721euec111@skcet.ac.in>
2023-04-04 21:18:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FruitSurvey from './components/Fruit';
+
+function App() {
+  return (
+    <div>
+      <FruitSurvey />
+    </div>
+  );
+}
+
+export default App;
